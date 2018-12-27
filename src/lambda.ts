90ee85fa01b083036ecffe8394c3efe0ad54f4fe
davidtom <davidjtomczyk@gmail.com>
2018-12-26 16:21:00 -0800

    fix encoding rules for aws serverless express middleware

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,28 +1,30 @@
 import * as awsServerlessExpress from 'aws-serverless-express';
 import app from './app';
 
-// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
+// NOTE(from repo): If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
 // due to a compressed response (e.g. gzip) which has not been handled correctly
 // by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
 // binaryMimeTypes below, then redeploy (`npm run package-deploy`)
+
+// NOTE: types listed below will be encoded when match between req.header.accept and res.header.contentType
 const binaryMimeTypes = [
-    'application/javascript',
-    'application/json',
-    'application/octet-stream',
-    'application/xml',
-    'font/eot',
-    'font/opentype',
-    'font/otf',
+    // 'application/javascript',
+    // 'application/json',
+    // 'application/octet-stream',
+    // 'application/xml',
+    // 'font/eot',
+    // 'font/opentype',
+    // 'font/otf',
     'image/jpeg',
     'image/png',
     'image/svg+xml',
-    'text/comma-separated-values',
-    'text/css',
-    'text/html',
-    'text/javascript',
-    'text/plain',
-    'text/text',
-    'text/xml'
+    // 'text/comma-separated-values',
+    // 'text/css',
+    // 'text/html',
+    // 'text/javascript',
+    // 'text/plain',
+    // 'text/text',
+    // 'text/xml'
 ]
 
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
